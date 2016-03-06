902d563238eed54800ed60b0bc92d1603ecdf64d
Piotr Krzemiński <pio.krzeminski@gmail.com>
2016-03-06 11:21:29 +0100

    json parse hotfix

--- src/main/scala/org/leanpoker/player/Player.scala
+++ src/main/scala/org/leanpoker/player/Player.scala
@@ -20,14 +20,18 @@ object Player {
     val myPlayer = players.toList.find(_.getAsJsonObject.get("name") == MyName).get
     val myCards = myPlayer.getAsJsonObject.get("hole_cards").getAsJsonArray.map(_.getAsJsonObject).toList
     myCards.map { cardJson =>
-      Card(cardJson.get("rank").getAsCharacter, cardJson.get("suits").getAsString)
+      Card(cardJson.get("rank").getAsString.head, cardJson.get("suits").getAsString)
     }
   }
+
+
 
 
   def betRequest(request: JsonElement) = {
 
     val myCards = getMyCards(request)
+
+
 
     if(myCards.groupBy(_.rank).values.exists(_.size == 2)) {
       100
